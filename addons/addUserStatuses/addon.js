ff089ec0cd7ccd0b5a4c8dec975eb2fb97dc22c7
imadeanaccount <138229538+imadeanaccount1@users.noreply.github.com>
2023-07-15 16:55:11 -0400

    Update addon.js

--- addons/addUserStatuses/addon.js
+++ addons/addUserStatuses/addon.js
@@ -1,59 +1,69 @@
-let new_nodes = 0;
-async function addon(reload) {
-    console.log("executing addon function", chrome.runtime.getURL("../utils.js"))
-        const utilsUrl = chrome.runtime.getURL("../utils.js");
-        const utils = await import(utilsUrl);
-        await utils.waitForElm("img.border-2")
-        // utils.observeUrlChange(addon(true));
-        console.log("pictures - waiting on feed", reload)
-        if (reload) {
-            await new Promise(resolve => setTimeout(resolve, 3000)); // 3 sec
+let newNodes = 0
+
+async function addon (reload) {
+  console.log('executing addon function', chrome.runtime.getURL('../utils.js'))
+  const utilsUrl = chrome.runtime.getURL('../utils.js')
+  const utils = await import(utilsUrl)
+  await utils.waitForElm('img.border-2')
+  // utils.observeUrlChange(addon(true));
+  console.log('pictures - waiting on feed', reload)
+  if (reload) {
+    await new Promise(resolve => setTimeout(resolve, 3000)) // 3 sec
+  }
+  await new Promise(resolve => setTimeout(resolve, 500)) // 3 sec
+  console.log('images', document.querySelectorAll('img.border-2'))
+  const observer = new MutationObserver(function (mutations) {
+    mutations.forEach(function (mutation) {
+      console.log('mutation', mutation.addedNodes.length, newNodes)
+      if (mutation.addedNodes.length > 0) {
+        newNodes += mutation.addedNodes.length
+        if (newNodes > 14) {
+          newNodes = 0
+          addon(true)
+          console.log('new posts added!!!')
         }
-        await new Promise(resolve => setTimeout(resolve, 500)); // 3 sec
-        console.log("images", document.querySelectorAll("img.border-2"))
-        const observer = new MutationObserver(function (mutations) {
-            mutations.forEach(function (mutation) {
-                console.log("mutation", mutation.addedNodes.length, new_nodes)
-                if (mutation.addedNodes.length > 0) {
-                    new_nodes += mutation.addedNodes.length;
-                    if (new_nodes > 14) {
-                        new_nodes = 0; 
-                        addon(true);
-                        console.log("new posts added!!!")
-                    }
+      }
+    })
+  })
 
-                }
-            });
-        });
+  console.log('finished setting up observer')
 
-        console.log("finished setting up observer")
+  console.log('observe')
+  // const loadMoreButton = document.querySelector("main > div > button.button-primary");
+  // loadMoreButton.addEventListener("click", addon(true));
 
-        
-        console.log("observe")
-        // const loadMoreButton = document.querySelector("main > div > button.button-primary");
-        // loadMoreButton.addEventListener("click", addon(true));
+  const profilePictures = document.querySelectorAll('img.border-2')
+  console.log('pictures', profilePictures)
 
-        const profilePictures = document.querySelectorAll("img.border-2");
-        console.log("pictures", profilePictures)
+  const onlineIndicator = document.createElement('div')
+  onlineIndicator.classList.add('onlineindicator1')
+  console.log('pictures')
 
-        const onlineIndicator = document.createElement("div");
-        onlineIndicator.classList.add("onlineindicator1");
-        console.log("pictures")
+  const setUserStatusesUrl = chrome.runtime.getURL('./addons/addUserStatuses/lib/setUserStatuses.js')
+  const contentMain = await import(setUserStatusesUrl)
+  contentMain.setUserStatuses(profilePictures, onlineIndicator)
 
-        const setUserStatusesUrl = chrome.runtime.getURL("./addons/addUserStatuses/lib/setUserStatuses.js");
-        const contentMain = await import(setUserStatusesUrl);
-        contentMain.setUserStatuses(profilePictures, onlineIndicator);
-
-        const config = { attributes: false, childList: true, characterData: false };
-        observer.observe(document.querySelector("main > div.my-4"), config)
+  const config = { attributes: false, childList: true, characterData: false }
+  observer.observe(document.querySelector('main > div.my-4'), config)
 }
 
 chrome.runtime.onMessage.addListener(
-    function (request, sender, sendResponse) {
-        if (request.greeting == "addUserStatuses")
-            sendResponse({ message: "hello" });
-    });
+  function (request, sender, sendResponse) {
+    if (request.greeting === 'addUserStatuses') { sendResponse({ message: 'hello' }) }
+  })
 
-    console.log("HIYA WORLD")
+window.addEventListener('message', function (event) {
+  // We only accept messages from ourselves
+  if (event.data.type && (event.data.type === 'FROM_PAGE')) {
+    console.log('Content script received: ' + event.data.text)
+  }
+})
 
-addon(false);
+const routeChangeScript = document.createElement('script')
+routeChangeScript.id = 'routeChangeScript'
+routeChangeScript.dataset.extensionId = chrome.runtime.id
+routeChangeScript.src = chrome.runtime.getURL('./addons/addUserStatuses/lib/routeChange.js')
+
+document.body.appendChild(routeChangeScript)
+
+addon(false)
