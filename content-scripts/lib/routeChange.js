6e7e760d602dd9c63313e78b49d790e676417741
imadeanaccount <138229538+imadeanaccount1@users.noreply.github.com>
2023-07-24 15:56:09 -0400

    Create routeChange.js

---
+++ content-scripts/lib/routeChange.js
@@ -0,0 +1,21 @@
+console.log('route change script loaded')
+
+$nuxt.$router.afterEach((to, from) => {
+  console.log('route changed')
+
+  //   const extensionId = document.querySelector('#routeChangeScript').dataset.extensionId
+  //   console.log('the extension id is ', extensionId)
+
+  //   chrome.runtime.sendMessage(extensionId, { recording_started: true },
+  //     function (response) {
+  //       console.log('sent@')
+  //     })
+
+  //   window.postMessage({
+  //     type: 'FROM_PAGE',
+  //     text: 'reload'
+  //   }, '*')
+
+  const event = new CustomEvent('PassToBackgroundRoute', { detail: 'HELLO!!!' })
+  window.dispatchEvent(event)
+})
