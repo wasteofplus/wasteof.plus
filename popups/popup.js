14a859d8916293f8a1aaf1c50f254b56c1196907
imadeanaccount <138229538+imadeanaccount1@users.noreply.github.com>
2023-08-02 19:36:55 -0400

    Fix Merge Conflicts

--- popups/popup.js
+++ popups/popup.js
@@ -327,11 +327,7 @@ fetch('templates/extensionCard.html').then(response => response.text()).then(asy
                 </div>
               </div>`
               const selectMiddleOptionText = `
-<<<<<<< HEAD
-              <button type="button" class="px-3 py-2 text-xxs font-medium text-gray-900 bg-white border-t border-b border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
-=======
               <button type="button" class="px-3 py-2 text-xs font-medium text-gray-900 bg-white border-t border-b border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
->>>>>>> port-for-firefox
                   Cha-Ching
                 </button>`
               const fileOptionText = `
