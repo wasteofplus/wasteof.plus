4845ddb71e4f2ce93c2e6f68ba3164e894ece08b
Scolder <69083943+ScolderCreations@users.noreply.github.com>
2023-08-09 09:50:33 -0700

    move the big boy where he belongs

--- addons/discordEmojiCodes/addon.js
+++ addons/discordEmojiCodes/addon.js
@@ -1,5 +1,5 @@
 function addon() {
-    const codesUrl = chrome.runtime.getURL('./codes.js')
+    const codesUrl = chrome.runtime.getURL('./lib/codes.js')
     const bigBoy = await import(codesUrl)
     // steal a bit from polls/addon.js
     document.querySelectorAll('.prose > pre').forEach(async (element) => {
