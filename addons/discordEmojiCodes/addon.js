7377f1e8a9ddfabfcb28053e7b5585c7054388bd
Scolder <69083943+ScolderCreations@users.noreply.github.com>
2023-08-09 09:45:24 -0700

    finish "" later "" stuff

--- addons/discordEmojiCodes/addon.js
+++ addons/discordEmojiCodes/addon.js
@@ -3,7 +3,14 @@ function addon() {
     const bigBoy = await import(codesUrl)
     // steal a bit from polls/addon.js
     document.querySelectorAll('.prose > pre').forEach(async (element) => {
-        const rawContent = element.textContent
-        // finish later
+        let rawContent = element.textContent
+        var afterConvert = rawContent
+        // search for potential emojis and replace them with counterparts
+        rawContent.matchAll(/\:[A-Za-z0-9\+\-]+\:/g).forEach( (emoji) => {
+            if (bigBoy.codes[emoji.substring(1,-1)]) {
+               afterConvert = afterConvert.replaceAll(emoji, bigBoy.codes[emoji.substring(1,-1)])
+            } else return
+        })
+        element.textContent = afterConvert // does this work???
     })
 }
