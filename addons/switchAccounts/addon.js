004a25dab56d78f8baedfec29b6c818252d97e12
imadeanaccount1 <imadeanaccount@proton.me>
2023-08-28 21:22:36 -0400

    fix positioning

--- addons/switchAccounts/addon.js
+++ addons/switchAccounts/addon.js
@@ -10,8 +10,8 @@ function addon () {
 `)
 
       topUsername.insertAdjacentHTML('afterEnd', `
-      <a href="#" id="accountListLink" class="absolute">
-<div id="accountsList" class="hidden bg-gray-100 dark:bg-gray-700 px-2 py-2 my-8 border-4 dark:border-gray-600 rounded-xl">
+      <a href="#" id="accountListLink" class="absolute my-8">
+<div id="accountsList" class="hidden bg-gray-100 dark:bg-gray-700 px-2 py-2 border-4 dark:border-gray-600 rounded-xl">
 <svg class="mb-2 inline-block" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="M627-210q17-33 26-69.5t9-75.5q0-80-35-146.5T532-612l-92 92v-320h320l-92 92q52 47 83 112t31 141q0 91-42.5 165T627-210Zm-427 90 92-92q-53-47-83.5-112T178-465q0-91 42.5-165T334-750q-17 33-26.5 69.5T298-605q0 80 35.5 146.5T428-348l92-92v320H200Z"/></svg>
 <p id="switchLabel" class="inline-block">Switch account</p>
 <button id="addAccountToSwitcher" class="dark:bg-gray-800 mt-4 pl-2 px-4 dark:hover:bg-gray-7
