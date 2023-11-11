eb3b12636cf8f205fd82f3d6a0d2c7ab2130e82a
imadeanaccount1 <imadeanaccount@proton.me>
2023-11-11 17:40:36 -0500

    fix navigation glitch

--- addons/twemojiEverywhere/addon.js
+++ addons/twemojiEverywhere/addon.js
@@ -6,6 +6,18 @@ twemojiScript.src = chrome.runtime.getURL('../../node_modules/twemoji/dist/twemo
 document.head.appendChild(twemojiScript)
 
 const twemojiScript1 = document.createElement('script')
-twemojiScript1.id = 'twemojiScript1'
 twemojiScript1.src = chrome.runtime.getURL('addons/twemojiEverywhere/lib/twemojiSet.js')
 document.head.appendChild(twemojiScript1)
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  import(chrome.runtime.getURL('../debug.js')).then((debug) => {
+    debug.log('twemoji everywhere message', message)
+    sendResponse({ message: 'hello' })
+
+    if (message.greeting === 'twemojiEverywhere') {
+      const twemojiScript1 = document.createElement('script')
+      twemojiScript1.src = chrome.runtime.getURL('addons/twemojiEverywhere/lib/twemojiSet.js')
+      document.head.appendChild(twemojiScript1)
+    }
+  })
+})
