db8b4e089b918cd0b8fa7fc80aa00f93c27e5b86
imadeanaccount1 <imadeanaccount@proton.me>
2023-11-26 09:42:33 -0500

    make emojipicker/autocomplete work in comments

--- addons/emojiAutocomplete/lib/addAutocomplete.js
+++ addons/emojiAutocomplete/lib/addAutocomplete.js
@@ -17,85 +17,86 @@ async function search (value) {
   return results
 }
 
-const contenteditableDiv = document.querySelector('div.prose > div')
-const prose = document.querySelector('div.prose')
-prose.style.position = 'relative'
+const contenteditableDivs = document.querySelectorAll('div.prose > div')
 
 const pickerContainer = document.createElement('div')
+contenteditableDivs.forEach((contenteditableDiv) => {
+  const prose = contenteditableDiv.parentElement
+  prose.style.position = 'relative'
+  contenteditableDiv.addEventListener('input', async (event) => {
+    console.log('input')
+    const text = event.target.innerText
+    const lastColonIndex = text.lastIndexOf(':')
+    const lastSpaceIndex = text.slice(lastColonIndex + 1).indexOf(' ')
+    const selection = window.getSelection()
+    const anchornode = selection.anchorNode
+    const range = selection.getRangeAt(0)
+    const rect = range.getClientRects()[0]
+    const boxRect = prose.getBoundingClientRect()
+    const ptest = document.createElement('p')
 
-contenteditableDiv.addEventListener('input', async (event) => {
-  console.log('input')
-  const text = event.target.innerText
-  const lastColonIndex = text.lastIndexOf(':')
-  const lastSpaceIndex = text.slice(lastColonIndex + 1).indexOf(' ')
-  const selection = window.getSelection()
-  const anchornode = selection.anchorNode
-  const range = selection.getRangeAt(0)
-  const rect = range.getClientRects()[0]
-  const boxRect = document.querySelector('div.prose').getBoundingClientRect()
-  const ptest = document.createElement('p')
-
-  console.log(selection.anchorOffset, lastSpaceIndex)
-  if (lastColonIndex !== -1 && lastColonIndex !== text.length - 1 && (selection.anchorOffset <= lastSpaceIndex + 1 || lastSpaceIndex === -1) && text.charAt(lastColonIndex + 1) !== ' ') {
-    document.querySelectorAll('.placehold').forEach((element) => {
-      element.remove()
-    })
-
-    if (pickerContainer) {
-      pickerContainer.remove()
-      while (pickerContainer.lastElementChild) {
-        pickerContainer.removeChild(pickerContainer.lastElementChild)
-      }
-    }
-    pickerContainer.style.top = `${(rect.top - boxRect.top) + 32}px`
-
-    pickerContainer.classList.add('emoji-picker-container')
-    pickerContainer.classList.add(...('bg-gray-100 dark:bg-gray-800 px-2 mb-4 py-2 border-2 dark:border-gray-700 rounded-xl'.split(' ')))
-    contenteditableDiv.parentElement.appendChild(pickerContainer)
-    // eslint-disable-next-line no-undef
-
-    console.log(text.slice(lastColonIndex + 1, lastSpaceIndex !== -1 ? lastSpaceIndex + 1 : text.length))
-    if (await search(text.slice(lastColonIndex + 1, lastSpaceIndex !== -1 ? lastSpaceIndex + 1 : text.length)).length === 0) {
-      pickerContainer.remove()
-      while (pickerContainer.lastElementChild) {
-        pickerContainer.removeChild(pickerContainer.lastElementChild)
-      }
-    } else {
-      (await search(text.slice(lastColonIndex + 1, lastSpaceIndex !== -1 ? lastSpaceIndex + 1 : text.length))).forEach((emoji) => {
-        const emojiButton = document.createElement('em-emoji')
-        // <em-emoji id="+1" size="2em"></em-emoji>
-        emojiButton.set = 'twitter'
-        emojiButton.insertAdjacentHTML('beforeend', '<p class="emojilabel font-semibold ml-4">' + emoji + '</p>')
-        emojiButton.setAttribute('set', 'twitter')
-
-        emojiButton.classList.add('emoji-button', ...('dark:hover:bg-gray-700 hover:bg-gray-900 px-2 rounded-md'.split(' ')))
-        emojiButton.id = emoji.slice(1, -1)
-
-        pickerContainer.appendChild(emojiButton)
-        emojiButton.addEventListener('click', (event) => {
-          pickerContainer.remove()
-          const newText = text.slice(0, lastColonIndex) + emojiButton.querySelector('span > img').alt + (lastSpaceIndex !== -1 ? text.slice(lastSpaceIndex + 1) : '')
-          anchornode.data = newText
-        })
+    console.log(selection.anchorOffset, lastSpaceIndex)
+    if (lastColonIndex !== -1 && lastColonIndex !== text.length - 1 && (selection.anchorOffset <= lastSpaceIndex + 1 || lastSpaceIndex === -1) && text.charAt(lastColonIndex + 1) !== ' ') {
+      document.querySelectorAll('.placehold').forEach((element) => {
+        element.remove()
       })
-    }
-  } else if (pickerContainer) {
-    pickerContainer.remove()
-    while (pickerContainer.lastElementChild) {
-      pickerContainer.removeChild(pickerContainer.lastElementChild)
-    }
-    document.querySelectorAll('.placehold').forEach((element) => {
-      element.remove()
-    })
-    if (lastColonIndex !== -1 && selection.anchorOffset === lastColonIndex + 1 && lastSpaceIndex === -1) {
-      ptest.innerText = 'Search Emoji...'
-      ptest.classList.add('placehold')
-      ptest.style.top = `${(rect.top - boxRect.top - 14)}px`
-      ptest.style.left = `${(rect.left - boxRect.left + 2)}px`
 
-      contenteditableDiv.parentElement.appendChild(ptest)
+      if (pickerContainer) {
+        pickerContainer.remove()
+        while (pickerContainer.lastElementChild) {
+          pickerContainer.removeChild(pickerContainer.lastElementChild)
+        }
+      }
+      pickerContainer.style.top = `${(rect.top - boxRect.top) + 32}px`
+
+      pickerContainer.classList.add('emoji-picker-container')
+      pickerContainer.classList.add(...('bg-gray-100 dark:bg-gray-800 px-2 mb-4 py-2 border-2 dark:border-gray-700 rounded-xl'.split(' ')))
+      contenteditableDiv.parentElement.appendChild(pickerContainer)
+      // eslint-disable-next-line no-undef
+
+      console.log(text.slice(lastColonIndex + 1, lastSpaceIndex !== -1 ? lastSpaceIndex + 1 : text.length))
+      if (await search(text.slice(lastColonIndex + 1, lastSpaceIndex !== -1 ? lastSpaceIndex + 1 : text.length)).length === 0) {
+        pickerContainer.remove()
+        while (pickerContainer.lastElementChild) {
+          pickerContainer.removeChild(pickerContainer.lastElementChild)
+        }
+      } else {
+        (await search(text.slice(lastColonIndex + 1, lastSpaceIndex !== -1 ? lastSpaceIndex + 1 : text.length))).forEach((emoji) => {
+          const emojiButton = document.createElement('em-emoji')
+          // <em-emoji id="+1" size="2em"></em-emoji>
+          emojiButton.set = 'twitter'
+          emojiButton.insertAdjacentHTML('beforeend', '<p class="emojilabel font-semibold ml-4">' + emoji + '</p>')
+          emojiButton.setAttribute('set', 'twitter')
+
+          emojiButton.classList.add('emoji-button', ...('dark:hover:bg-gray-700 hover:bg-gray-900 px-2 rounded-md'.split(' ')))
+          emojiButton.id = emoji.slice(1, -1)
+
+          pickerContainer.appendChild(emojiButton)
+          emojiButton.addEventListener('click', (event) => {
+            pickerContainer.remove()
+            const newText = text.slice(0, lastColonIndex) + emojiButton.querySelector('span > img').alt + (lastSpaceIndex !== -1 ? text.slice(lastSpaceIndex + 1) : '')
+            anchornode.data = newText
+          })
+        })
+      }
+    } else if (pickerContainer) {
+      pickerContainer.remove()
+      while (pickerContainer.lastElementChild) {
+        pickerContainer.removeChild(pickerContainer.lastElementChild)
+      }
+      document.querySelectorAll('.placehold').forEach((element) => {
+        element.remove()
+      })
+      if (lastColonIndex !== -1 && selection.anchorOffset === lastColonIndex + 1 && lastSpaceIndex === -1) {
+        ptest.innerText = 'Search Emoji...'
+        ptest.classList.add('placehold')
+        ptest.style.top = `${(rect.top - boxRect.top - 14)}px`
+        ptest.style.left = `${(rect.left - boxRect.left + 2)}px`
+
+        contenteditableDiv.parentElement.appendChild(ptest)
+      }
     }
-  }
+  })
 })
 // contenteditableDiv.addEventListener('input', (event) => {
 //   const text = event.target.innerText
--- addons/emojiPicker/lib/addPicker.js
+++ addons/emojiPicker/lib/addPicker.js
@@ -1,68 +1,78 @@
 const modalHeader1 = [...document.querySelectorAll('#modal-header')]
   .find((el) => el.textContent === 'Create post')
   .parentElement.querySelector('#modal-header ~ div > div > div > div')
-const insertPollsButton = document.createElement('button')
-insertPollsButton.classList.add(
-  'emojiActionButton',
-  'text-white',
-  'p-1',
-  'rounded',
-  'bg-gray-500'
-)
-const insertPollsButtonSpan = document.createElement('span')
-insertPollsButtonSpan.innerHTML = `
+const allModalHeaders = [modalHeader1, ...[...document.querySelector('div.container > main > .max-w-2xl').querySelectorAll('div[contenteditable="true"]')].map((el) => el.parentElement.parentElement.firstChild)]
+console.log('modals', modalHeader1, allModalHeaders)
+
+allModalHeaders.forEach((modalHeader) => {
+  const insertPollsButton = document.createElement('button')
+  const prose = modalHeader.parentElement.querySelector('div.prose')
+  insertPollsButton.classList.add(
+    'emojiActionButton',
+    'text-white',
+    'p-1',
+    'rounded',
+    'bg-gray-500'
+  )
+  const insertPollsButtonSpan = document.createElement('span')
+  insertPollsButtonSpan.innerHTML = `
   <svg class="emojiAddButton text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.408 7.5h.01m-6.876 0h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0ZM4.6 11a5.5 5.5 0 0 0 10.81 0H4.6Z"/>
   </svg>`
-insertPollsButton.appendChild(insertPollsButtonSpan)
-insertPollsButton.addEventListener('click', (event) => {
-  console.log(event.target)
-  if (event.target.classList.contains('emojiAddButton')) {
-    picker.style.display = picker.style.display === 'none' ? 'flex' : 'none'
-  }
-})
-modalHeader1.insertBefore(insertPollsButton, modalHeader1.lastChild)
+  insertPollsButton.appendChild(insertPollsButtonSpan)
+  insertPollsButton.addEventListener('click', (event) => {
+    console.log(event.target)
+    if (event.target.classList.contains('emojiActionButton') || event.target.classList.contains('emojiAddButton')) {
+      picker.style.display = picker.style.display === 'none' ? 'flex' : 'none'
+    }
+  })
+  modalHeader.insertBefore(insertPollsButton, modalHeader.lastChild)
 
-const pickerOptions = {
-  set: 'twitter',
-  theme: document.documentElement.className === 'dark' ? 'dark' : 'light',
-  onEmojiSelect: (emoji) => {
-    console.log('select')
-    const selection = window.getSelection()
-    console.log(selection.anchorNode)
-    if (document.querySelector('div.prose > div').contains(selection.anchorNode)) {
+  const pickerOptions = {
+    set: 'twitter',
+    theme: document.documentElement.className === 'dark' ? 'dark' : 'light',
+    onEmojiSelect: (emoji) => {
+      console.log('select')
+      const selection = window.getSelection()
+      console.log(selection.anchorNode)
+      // if (prose.querySelector('div').contains(selection.anchorNode)) {
       console.log('contains', selection.anchorNode)
-      if (selection.anchorNode.classList && selection.anchorNode.classList.contains('is-empty')) {
-        const p = document.createElement('p')
-        p.innerText = emoji.native
-        selection.anchorNode.parentElement.appendChild(p)
-        selection.anchorNode.remove('is-empty')
-      } else if (selection.anchorNode.classList && selection.anchorNode.classList.contains('ProseMirror')) {
-        console.log('prosemirror', selection.anchorNode.querySelectorAll('p')[selection.anchorNode.querySelectorAll('p').length - 1])
-        // const p = document.createElement('p')
-        // p.innerText = emoji.native
-        if (selection.anchorNode.querySelectorAll('p').length > 0) {
-          selection.anchorNode.querySelectorAll('p')[selection.anchorNode.querySelectorAll('p').length - 1].innerText = selection.anchorNode.querySelectorAll('p')[selection.anchorNode.querySelectorAll('p').length - 1].innerText + emoji.native
+      if ((document.querySelector('#__layout > div > div').contains(selection.anchorNode) && document.querySelector('#__layout > div > div').contains(modalHeader)) || (modalHeader.parentElement.contains(selection.anchorNode))) {
+        if (selection.anchorNode.classList && selection.anchorNode.classList.contains('is-empty')) {
+          const p = document.createElement('p')
+          p.innerText = emoji.native
+          selection.anchorNode.parentElement.appendChild(p)
+          selection.anchorNode.remove('is-empty')
+        } else if (selection.anchorNode.classList && selection.anchorNode.classList.contains('ProseMirror')) {
+          console.log('prosemirror', selection.anchorNode.querySelectorAll('p')[selection.anchorNode.querySelectorAll('p').length - 1])
+          // const p = document.createElement('p')
+          // p.innerText = emoji.native
+          if (selection.anchorNode.querySelectorAll('p').length > 0) {
+            selection.anchorNode.querySelectorAll('p')[selection.anchorNode.querySelectorAll('p').length - 1].innerText = selection.anchorNode.querySelectorAll('p')[selection.anchorNode.querySelectorAll('p').length - 1].innerText + emoji.native
+          }
+          // selection.anchorNode.remove('is-empty')
+        } else {
+          selection.anchorNode.data = selection.anchorNode.data + emoji.native
         }
-        // selection.anchorNode.remove('is-empty')
       } else {
-        selection.anchorNode.data = selection.anchorNode.data + emoji.native
+        prose.querySelector('div').querySelectorAll('p')[selection.anchorNode.querySelectorAll('p').length - 1].innerText = selection.anchorNode.querySelectorAll('p')[selection.anchorNode.querySelectorAll('p').length - 1].innerText + emoji.native
       }
-    } else {
-      console.log('does not contain')
+      // } else {
+      //   console.log('does not contain')
+      // }
     }
   }
-}
 
-// eslint-disable-next-line no-undef
-const picker = new EmojiMart.Picker(pickerOptions)
-picker.addEventListener('click', () => {
-  console.log('click')
+  // eslint-disable-next-line no-undef
+  const picker = new EmojiMart.Picker(pickerOptions)
+  picker.addEventListener('click', () => {
+    console.log('click')
+  })
+  picker.classList.add('actual-picker')
+  insertPollsButton.appendChild(picker)
+  document.querySelector('div > div > div > div > div > div.vfm__content.relative.flex.flex-col.max-h-full.mx-4.rounded > div').style.overflowY = 'visible'
+
+  setTimeout(() => {
+    picker.style.display = 'none'
+  }, 1000)
 })
-picker.classList.add('actual-picker')
-document.querySelector('.emojiActionButton').appendChild(picker)
-document.querySelector('div > div > div > div > div > div.vfm__content.relative.flex.flex-col.max-h-full.mx-4.rounded > div').style.overflowY = 'visible'
-
-setTimeout(() => {
-  picker.style.display = 'none'
-}, 1000)
